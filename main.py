f4cd376165a26c6f6889b3b76b4a8470dfa67e2f
Shah77966 <shahkhalid.77966@gmail.com>
2024-10-13 01:44:23 +0530

    added cv2 write image function

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision
 from torchvision.models.detection.rpn import AnchorGenerator
 from torchvision.transforms import functional as F
-
 import albumentations as A # Library for augmentations
 
 # https://github.com/pytorch/vision/tree/main/references/detection
@@ -123,47 +122,6 @@ batch = next(iterator)
 # print("Transformed targets:\n", batch[1])
 
 keypoints_classes_ids2names = {0: 'Head', 1: 'Tail'}
-"""
-def visualize(image, bboxes, keypoints, image_original=None, bboxes_original=None, keypoints_original=None):
-    print("Visualizing")
-    fontsize = 18
-
-    for bbox in bboxes:
-        start_point = (bbox[0], bbox[1])
-        end_point = (bbox[2], bbox[3])
-        image = cv2.rectangle(image.copy(), start_point, end_point, (0,255,0), 2)
-    
-    for kps in keypoints:
-        for idx, kp in enumerate(kps):
-            image = cv2.circle(image.copy(), tuple(kp), 5, (255,0,0), 10)
-            image = cv2.putText(image.copy(), " " + keypoints_classes_ids2names[idx], tuple(kp), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 3, cv2.LINE_AA)
-
-    if image_original is None and keypoints_original is None:
-        plt.figure(figsize=(40,40))
-        plt.imshow(image)
-
-    else:
-        for bbox in bboxes_original:
-            start_point = (bbox[0], bbox[1])
-            end_point = (bbox[2], bbox[3])
-            image_original = cv2.rectangle(image_original.copy(), start_point, end_point, (0,255,0), 2)
-        
-        for kps in keypoints_original:
-            for idx, kp in enumerate(kps):
-                image_original = cv2.circle(image_original, tuple(kp), 5, (255,0,0), 10)
-                image_original = cv2.putText(image_original, " " + keypoints_classes_ids2names[idx], tuple(kp), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 3, cv2.LINE_AA)
-
-        f, ax = plt.subplots(1, 2, figsize=(40, 20))
-
-        ax[0].imshow(image_original)
-        ax[0].set_title('Original image', fontsize=fontsize)
-
-        ax[1].imshow(image)
-        ax[1].set_title('Transformed image', fontsize=fontsize)
-        plt.show()
-"""  
-import cv2
-import os
 
 def visualize(image, bboxes, keypoints, save_path, image_original=None, bboxes_original=None, keypoints_original=None):
     print("Visualizing and Saving")
